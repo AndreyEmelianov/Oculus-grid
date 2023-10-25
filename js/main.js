523975349f1f11163a0ee05f26dcd9b47401529d
Andrey Emelianov <621077@mail.ru>
2023-10-25 16:53:10 +0300

    [feat(main.js)]: реализовал простую логику переключения табов

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
-$(function(){
+'use strict';
 
-});
+// tabs
+const tabItems = document.querySelectorAll('.tabs__btn-item');
+const tabContents = document.querySelectorAll('.tabs__content-item');
+
+const open = (e) => {
+  const tabTarget = e.currentTarget;
+  const button = tabTarget.dataset.button;
+
+  tabItems.forEach((item) => item.classList.remove('tabs__btn-item--active'));
+  tabContents.forEach((content) => content.classList.remove('tabs__content-item--active'));
+
+  tabTarget.classList.add('tabs__btn-item--active');
+  document.querySelector(`#${button}`).classList.add('tabs__content-item--active');
+};
+
+tabItems.forEach((tabItem) => tabItem.addEventListener('click', open));
